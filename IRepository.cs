f9479f0df7677a5e8e7b1be14432b8017af5d206
Lexi <82426264+LexiCC@users.noreply.github.com>
2022-03-19 01:06:01 -0400

    Add files via upload

---
+++ IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpAssignment
+{
+    public interface IRepository<T> where T : class
+    {
+        //1.void Add(T item)
+        public void Add(T item);
+
+        //2.void Remove(T item)
+        public void Remove(T item);
+
+        //3.Void Save()
+        public void Save();
+
+        //4.IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll();
+
+        //5.T GetById(int id)
+        public Entity GetById(int id);
+    }
+}
